ca4ea35212936941f25cb6e3b1fc8ac1d974b0ae
Anna <studyforal@gmail.com>
2024-03-06 17:36:38 +0100

    Add project

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,12 @@
+#Package initialization
+
 import os
 
 from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+# instance_relative_config == '.../KSDFKKJSKFJKDJF/SDKFSDF/ODJFSD/__NAME__'
     app.config.from_mapping(
         SECRET_KEY = 'dev',
         DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -19,11 +22,18 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/hello')
+    @app.route('/hello') # flask decorator that indicates a URL
     def hello():
         return 'Hello, World!'
 
-    from . import db
+    from . import db # import internal module
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
